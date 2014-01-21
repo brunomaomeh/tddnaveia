e8219e853aac9cdad643e6de3ae40e11e4fc721f
Matheus Fechine <matheusfmoura@gmail.com>
2014-01-21 17:38:00 -0300

    Implementando paginas

--- src/main/java/br/com/tddnaveia/controller/UsuarioController.java
+++ src/main/java/br/com/tddnaveia/controller/UsuarioController.java
@@ -13,28 +13,30 @@ import br.com.tddnaveia.model.Usuario;
 import br.com.tddnaveia.service.UsuarioService;
 
 @Resource
-@Path("/usuario")
+@Path("")
 public class UsuarioController {
 
 	private Result result;
 	private UsuarioService service;
-	private Validator validator;
+//	private Validator validator;
 	
-	public UsuarioController(Result result, UsuarioService service, Validator validator) {
+	public UsuarioController(Result result, UsuarioService service
+//			, Validator validator
+			) {
 		this.result = result;
 		this.service = service;
-		this.validator = validator;
+//		this.validator = validator;
 	}
 	
 	@Get
-	@Path("/carrega")
+	@Path("/usuario/carrega")
 	public void carregaUm(Usuario usuario) {
 		Usuario usuarioRetornado = service.carregaUm(usuario);
 		result.include("usuario", usuarioRetornado);
 	}
 	
 	@Post
-	@Path("/cadastra")
+	@Path("/usuario/cadastra")
 	public void cadastraUm(final Usuario usuario) {
 //	 	validator.checking(new Validations() {{
 //	        that(!usuario.getNome().isEmpty(),  "erro", "Nome nao informado");
@@ -47,10 +49,11 @@ public class UsuarioController {
 		} catch (UsuarioCadastradoException e) {
 			result.include("erro", "Usuario já existe!");
 		}
+		result.redirectTo(this).paginaDeCadastro();
 	}
 
 	@Get
-	@Path("/lista")
+	@Path("/usuario/lista")
 	public void listarPor(Usuario usuario) {
 		List<Usuario> usuarios = service.listarPor(usuario);
 		if(usuarios!=null){
@@ -61,23 +64,28 @@ public class UsuarioController {
 	}
 	
 	@Post
-	@Path("/exclui")
+	@Path("/usuario/exclui")
 	public void excluiUm(Usuario usuario) {
 		service.excluiUm(usuario);
 		result.include("sucesso", "Usuário excluído com sucesso!");
 	}
 
 	@Post
-	@Path("/edita")
+	@Path("/usuario/edita")
 	public void editaUm(Usuario usuario) {
 		service.editaUm(usuario);
 		result.include("sucesso", "Usuário editado com sucesso!");
 	}
 
 	@Get
-	@Path("/")
+	@Path("/usuario/")
 	public void paginaDeCadastro(){
 		
 	}
 	
+	@Get
+	@Path("/")
+	public void index(){
+		
+	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/usuario/index.jsp
@@ -0,0 +1,6 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<body>
+	<a href="/usuario/"> Página de Cadastro</a>
+</body>
+</html>
